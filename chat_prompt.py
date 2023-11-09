1233d37232896412d0f431c466d57cda5436c72e
Umemura Kanzaki <85058029+dominhtan@users.noreply.github.com>
2023-11-09 10:20:35 +0700

    Create chat_template.py

---
+++ chat_prompt.py
@@ -0,0 +1,21 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from langchain.prompts.chat import (
+    ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+)
+chat = ChatOpenAI(openai_api_key="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")
+template = "###"
+system_prompt = SystemMessagePromptTemplate.from_template(template)
+human = "### {var} ### {var2}"
+human_prompt = HumanMessagePromptTemplate.from_template(human)
+
+chat_prompt = ChatPromptTemplate.from_messages([system_prompt,human_prompt])
+chat_prompt.format_messages(var="###", var2="###")
+conversation = LLMChain(
+    llm=chat,
+    prompt=chat_prompt,
+    verbose=True,
+)
+print(conversation(chat_prompt))
